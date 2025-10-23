d2e6ec2601b8be79bdf3e9d8fcf558695fb41a56
adis654 <adisnabil801@gmail.com>
2025-10-23 19:16:47 +0700

    Add CategoryPage component with health tips

---
+++ CategoryPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CategoryPage = () => {
+    return (
+        <div>
+            <h2>Solusi untuk Masalah Kesehatan</h2>
+            <ul>
+                <li><a href="/artikel/makan-sehat">Tips Makan Sehat</a></li>
+                <li><a href="/artikel/olahraga-rutin">Pentingnya Olahraga Rutin</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default CategoryPage;
